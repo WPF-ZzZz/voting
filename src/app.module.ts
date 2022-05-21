b38f8c5308284dfc25370d7ef4713e251e07b06c
Fei <feiwong.developer@gmail.com>
2022-05-21 23:01:15 +0800

    Remove no use files generated from the nestjs project at the beginning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PollsModule } from './polls/polls.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AnswersModule } from './answers/answers.module';
@@ -8,7 +6,7 @@ import { VotesModule } from './votes/votes.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://Fei:27017,Fei:27018,Fei:27019/simple-voting?replicaSet=rs'), PollsModule, AnswersModule, VotesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
